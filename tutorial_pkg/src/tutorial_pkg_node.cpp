5e53dc2a027ec6d8d20d26933db27bb7d986971d
Hard-won <88823467+hard-won@users.noreply.github.com>
2022-12-09 08:02:08 +0000

    Add files via upload

--- tutorial_pkg/src/tutorial_pkg_node.cpp
+++ tutorial_pkg/src/tutorial_pkg_node.cpp
@@ -17,6 +17,27 @@ using namespace std;
 ofstream odomTrajFile; // Declare a file object to record odometry data.
 ofstream odomVelFile; // Declare a file object to record velocity data.
 ofstream laserFile; // Declare a file object for recording your laser data.
+ofstream laserMapFile;
+struct EulerAngles{double roll, pitch, yaw;}; // yaw is what you want, i.e. Th
+struct Quaternion{double w, x, y, z;};
+EulerAngles ToEulerAngles(Quaternion q){ // for calculating Th
+EulerAngles angles;
+// roll (x-axis rotation)
+double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
+double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
+angles.roll = atan2(sinr_cosp, cosr_cosp);
+// pitch (y-axis rotation)
+double sinp = +2.0 * (q.w * q.y - q.z * q.x);
+if (fabs(sinp) >= 1)
+angles.pitch = copysign(M_PI/2, sinp); //use 90 degrees if out of range
+else
+angles.pitch = asin(sinp);
+// yaw (z-axis rotation)
+double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
+double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+angles.yaw = atan2(siny_cosp, cosy_cosp);
+return angles;
+}
 class Stopper : public rclcpp::Node{
 public:
 /* velocity control variables*/
@@ -53,6 +74,12 @@ int numberOfCycle=0;
 void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
 double frontRange, mleftRange, leftRange, rightRange, mrightRange;
 int laser_index = 0; // index the laser scan data
+Quaternion robotQuat;
+EulerAngles robotAngles;
+double robotHeadAngle;
+double leftAngle=M_PI/2, mleftAngle=M_PI/4, frontAngle=0;
+double mrightAngle=-M_PI/4, rightAngle=-M_PI/2;
+void transformMapPoint(ofstream& fp, double laserRange, double laserTh, double robotTh, double robotX, double robotY);
 private:
 // Publisher to the robot's velocity command topic
 rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
@@ -114,6 +141,12 @@ moveStop();}
 robVelocity = odomMsg->twist.twist.linear.x;
 odomVelFile << numberOfCycle++ << " " << robVelocity << endl;
 odomTrajFile<< PositionX <<" "<< PositionY<<endl;
+robotQuat.x = odomMsg->pose.pose.orientation.x;
+robotQuat.y = odomMsg->pose.pose.orientation.y;
+robotQuat.z = odomMsg->pose.pose.orientation.z;
+robotQuat.w = odomMsg->pose.pose.orientation.w;
+robotAngles = ToEulerAngles(robotQuat);
+robotHeadAngle = robotAngles.yaw;
 }
 void Stopper::scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan)
 {
@@ -123,24 +156,46 @@ frontRange = scan->ranges[200]; // get a range reading at the front angle
 mrightRange = scan->ranges[150]; // get a range reading at the front-right angle
 rightRange = scan->ranges[100]; // get the range reading at the right angle
 laserFile << leftRange << ","<< mleftRange << "," << frontRange<<"," << mrightRange << "," << rightRange <<"," <<laser_index++<< endl;
+transformMapPoint(laserMapFile,frontRange,frontAngle,robotHeadAngle,PositionX, PositionY);
+transformMapPoint(laserMapFile, mleftRange, mleftAngle, robotHeadAngle,
+PositionX, PositionY);
+transformMapPoint(laserMapFile, leftRange, leftAngle, robotHeadAngle,
+PositionX, PositionY);
+transformMapPoint(laserMapFile, rightRange, rightAngle, robotHeadAngle,
+PositionX, PositionY);
+transformMapPoint(laserMapFile, mrightRange, mrightAngle, robotHeadAngle,
+PositionX, PositionY);
+}
+void Stopper::transformMapPoint(ofstream& fp, double laserRange, double laserTh, double robotTh, double robotX, double robotY){
+double transX, transY;
+transX = laserRange * cos(robotTh + laserTh) + robotX;
+transY = laserRange * sin(robotTh + laserTh) + robotY;
+if (transX < 0) transX = 0;
+if (transY < 0) transY = 0;
+fp << transX << ", " << transY << endl;
 }
 void Stopper::startMoving(){
 odomVelFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/odomVelData.csv",ios::trunc);
 odomTrajFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/odomTrajData.csv",ios::trunc); //note you should modify hhu to your username
 laserFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/laserData.csv",ios::trunc);
+laserMapFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/laserMapData.csv",ios::trunc);
 
 while (rclcpp::ok()){ // keep spinning loop until user presses Ctrl+C
 RCLCPP_INFO(this->get_logger(), "Start moving");
 rclcpp::WallRate loop_rate(20);
 while (rclcpp::ok()){
-auto node = std::make_shared<Stopper>();
+while (rclcpp::ok()){ // keep spinning loop until you press Ctrl+C
+auto node = std::make_shared<Stopper>();{
 rclcpp::spin(node); // update
 // RCLCPP_INFO(this->get_logger(), "Robot speed: %f",FORWARD_SPEED_LOW );
 loop_rate.sleep(); // wait delta time
 }
+}
 odomVelFile.close();
 odomTrajFile.close();
 laserFile.close();
+laserMapFile.close();
+}
 }
 }
 int main(int argc, char* argv[]){
