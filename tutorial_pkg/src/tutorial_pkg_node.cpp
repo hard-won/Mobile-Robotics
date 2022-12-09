5b506847b098702426783d204d94c9a55f8c1cd1
Hard-won <88823467+hard-won@users.noreply.github.com>
2022-12-09 04:15:02 +0000

    Add files via upload

--- tutorial_pkg/src/tutorial_pkg_node.cpp
+++ tutorial_pkg/src/tutorial_pkg_node.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <functional>
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include <memory>
 #include <string>
 #include "rclcpp/rclcpp.hpp"
@@ -15,6 +16,7 @@ using namespace std::chrono_literals;
 using namespace std;
 ofstream odomTrajFile; // Declare a file object to record odometry data.
 ofstream odomVelFile; // Declare a file object to record velocity data.
+ofstream laserFile; // Declare a file object for recording your laser data.
 class Stopper : public rclcpp::Node{
 public:
 /* velocity control variables*/
@@ -33,6 +35,8 @@ Stopper():Node("Stopper"), count_(0){
 publisher_ = this->create_publisher< geometry_msgs::msg::Twist>("cmd_vel", 10);
 odomSub_=this->create_subscription<nav_msgs::msg::Odometry>("odom",10,
 std::bind(&Stopper::odomCallback, this, std::placeholders::_1));
+laserScan_=this->create_subscription<sensor_msgs::msg::LaserScan>("scan",10,
+std::bind(&Stopper::scanCallback, this, std::placeholders::_1));
 };
 /* moving functions */
 void startMoving();
@@ -46,6 +50,9 @@ double odom_landmark1=1.20, odom_landmark1a=0.38, odom_landmark2=0.80;
 int stage=0;
 double robVelocity;
 int numberOfCycle=0;
+void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+double frontRange, mleftRange, leftRange, rightRange, mrightRange;
+int laser_index = 0; // index the laser scan data
 private:
 // Publisher to the robot's velocity command topic
 rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
@@ -53,6 +60,7 @@ rclcpp::TimerBase::SharedPtr timer_;
 size_t count_;
 //Subscriber to robot’s odometry topic
 rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odomSub_;
+rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laserScan_;
 };
 void Stopper::moveForward(double forwardSpeed){
 auto msg = geometry_msgs::msg::Twist();
@@ -107,21 +115,33 @@ robVelocity = odomMsg->twist.twist.linear.x;
 odomVelFile << numberOfCycle++ << " " << robVelocity << endl;
 odomTrajFile<< PositionX <<" "<< PositionY<<endl;
 }
+void Stopper::scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan)
+{
+leftRange = scan->ranges[300]; // get a range reading at the left angle
+mleftRange = scan->ranges[250]; // get a range reading at the front-left angle
+frontRange = scan->ranges[200]; // get a range reading at the front angle
+mrightRange = scan->ranges[150]; // get a range reading at the front-right angle
+rightRange = scan->ranges[100]; // get the range reading at the right angle
+laserFile << leftRange << ","<< mleftRange << "," << frontRange<<"," << mrightRange << "," << rightRange <<"," <<laser_index++<< endl;
+}
 void Stopper::startMoving(){
 odomVelFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/odomVelData.csv",ios::trunc);
 odomTrajFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/odomTrajData.csv",ios::trunc); //note you should modify hhu to your username
+laserFile.open("/ufs/servd01/users/sw19204/M-Drive/ros_workspace/src/tutorial_pkg/laserData.csv",ios::trunc);
 
+while (rclcpp::ok()){ // keep spinning loop until user presses Ctrl+C
 RCLCPP_INFO(this->get_logger(), "Start moving");
 rclcpp::WallRate loop_rate(20);
 while (rclcpp::ok()){
 auto node = std::make_shared<Stopper>();
 rclcpp::spin(node); // update
-// remove the following Green line
 // RCLCPP_INFO(this->get_logger(), "Robot speed: %f",FORWARD_SPEED_LOW );
 loop_rate.sleep(); // wait delta time
 }
 odomVelFile.close();
 odomTrajFile.close();
+laserFile.close();
+}
 }
 int main(int argc, char* argv[]){
 rclcpp::init(argc, argv);
